9b18b5a8791a4c9ef32357b51ccd68f622d0fb5b
zhengjinf <1838307288@qq.com>
2018-08-18 16:25:03 +0800

    提交

--- public/back/js/login.js
+++ public/back/js/login.js
@@ -1,15 +1,14 @@
-//1. 用户名不能为空
-//2. 用户密码不能为空
-//3. 用户密码长度为6-12位
-
+//1. 用户名不能为空,长度2-6位
+//2. 用户密码不能为空,长度为6-12位
 
 $(function(){
+  //1.校验功能
   $("#form").bootstrapValidator({
      //设置小图标
      feedbackIcons: {
-      valid: 'glyphicon glyphicon-ok',
-      invalid: 'glyphicon glyphicon-remove',
-      validating: 'glyphicon glyphicon-refresh'
+      valid: 'glyphicon glyphicon-ok',//校验成功
+      invalid: 'glyphicon glyphicon-remove',//校验失败
+      validating: 'glyphicon glyphicon-refresh'//校验中
     },
     //配置字段
     fields:{
@@ -42,4 +41,35 @@ $(function(){
       },
     }
   })
+
+  //2.注册表单校验成功的事件,阻止submit提交的默认跳转行为
+  $("#form").on('success.form.bv',function(e){
+    e.preventDefault();
+    // console.log('校验成功,默认行为被阻住了');
+    $.ajax({
+      type:'post',
+      url:'/employee/employeeLogin',
+      data:$('#form').serialize(),
+      dataType:'json',
+      success:function(info){
+        // console.log(info);
+        if(info.success){
+          location.href="index.html"
+        }
+        if(info.error===1000){
+          alert ("密码错误");
+        }
+        if(info.error===1001){
+          alert ("密码错误");
+        }
+      }
+    })
+  })
+
+  //2.重置校验状态
+
+
+
+
+
 })
